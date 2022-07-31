c0e0bb12079dfcaa420f03f266802026017c17bd
Eibenjs <eminsnl2@gmail.com>
2022-07-31 18:41:59 +0300

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { useState } from 'react'
 
+import { Nui } from './nui/nui.component'
+
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import CartProvider from './store/CartProvider'
 import Order from './components/Orders/Order'
 
 function App() {
+  const [hidden, setHidden] = useState(true)
   const [cartIsShown, setCartIsShown] = useState(false)
   const [orderIsShown, setOrderIsShown] = useState(false)
+
+  const closePage = () => {
+    setHidden(true)
+    Nui.send('exit_focus', {}) // kapatma yollanilan
+  }
+
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      closePage()
+    }
+  })
+
+  window.addEventListener('message', function (event) {
+    const type = event.data.type
+
+    if (type === 'SHOW_PAGE') {
+      // acilirken yollanan
+      setHidden(false)
+    } else if (type === 'CLOSE_PAGE') {
+      closePage()
+    }
+  })
 
   const showCartHandler = () => {
     setCartIsShown(true)
@@ -26,16 +51,18 @@ function App() {
   }
 
   return (
-    <CartProvider>
-      <Header onShowOrder={toggleOrder} onShowCart={showCartHandler} />
-      <main>
-        {orderIsShown ? (
-          <Order />
-        ) : (
-          <Meals cartOnClose={hideCartHandler} cartIsShown={cartIsShown} />
-        )}
-      </main>
-    </CartProvider>
+    <>
+      <CartProvider hidden={hidden}>
+        <Header onShowOrder={toggleOrder} onShowCart={showCartHandler} />
+        <main>
+          {orderIsShown ? (
+            <Order />
+          ) : (
+            <Meals cartOnClose={hideCartHandler} cartIsShown={cartIsShown} />
+          )}
+        </main>
+      </CartProvider>
+    </>
   )
 }
 
