872bd0e9a37fd2120d39845566936417dac908e5
SirYavuz <51060721+SirYavuz@users.noreply.github.com>
2022-07-31 20:05:43 +0300

    lua files

---
+++ config.lua
@@ -0,0 +1,53 @@
+config = {}
+
+config.burgershotCategories = {
+    [1] = {name = "Burger", "hamburger"},
+    [2] = {name = "Tatlı", "tatli"},
+    [3] = {name = "İçecek", "icecek"},
+    [4] = {name = "Kızartma", "kizartma"}
+}
+
+config.burgershotMeals = {
+    [1] = {
+        id = "moneyshot",
+        name = "Moneyshot",
+        description = "Cok guzel hamburger..",
+        category = "burger",
+        altPic = "https://cdn.discordapp.com/attachments/833414619254620260/999681032796241920/zorrtatak.png" 
+    },
+    [2] = {
+        id = "fowlburger",
+        name = "Fowl Burger",
+        description = "Cok guzel hamburger fowl..",
+        category = "burger",
+        altPic = "https://cdn.discordapp.com/attachments/833414619254620260/999681032796241920/zorrtatak.png" 
+    },
+    [3] = {
+        id = "heartstopper",
+        name = "Heartstopper",
+        description = "Cok guzel hamburger heartstopper..",
+        category = "burger",
+        altPic = "https://cdn.discordapp.com/attachments/833414619254620260/999681032796241920/zorrtatak.png" 
+    },
+    [4] = {
+        id = "kizartma",
+        name = "Patates kızartması",
+        description = "Cok guzel patates kızartması..",
+        category = "kizartma",
+        altPic = "https://cdn.discordapp.com/attachments/833414619254620260/999681032796241920/zorrtatak.png" 
+    },
+    [5] = {
+        id = "slushy",
+        name = "Slushy",
+        description = "slushy o..",
+        category = "tatli",
+        altPic = "https://cdn.discordapp.com/attachments/833414619254620260/999681032796241920/zorrtatak.png" 
+    },
+    [5] = {
+        id = "bskola",
+        name = "Burgershot Kola",
+        description = "bs ozel kola..",
+        category = "icecek",
+        altPic = "https://cdn.discordapp.com/attachments/833414619254620260/999681032796241920/zorrtatak.png" 
+    }
+}
